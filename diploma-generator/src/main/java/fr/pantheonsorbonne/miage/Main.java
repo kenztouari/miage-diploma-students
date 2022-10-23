8dc2a6ce9eb17a150a2690cd0f5364f151d94c5c
Kenz Touari <kenztouari@icloud.com>
2022-10-23 19:16:13 +0200

    releasev1

---
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -0,0 +1,20 @@
+package fr.pantheonsorbonne.miage;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.google.common.collect.Iterables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Path studentDbPath = Paths.get("student-repository", "src", "main", "resources", "students.db");
+		StudentRepository repo = StudentRepository.withDB(studentDbPath.toString());
+		Student student = Iterables.get(repo, 0);
+
+		MiageDiplomaGenerator generator = new MiageDiplomaGenerator(student);
+		DiplomaFileAdapter adapter = new DiplomaFileAdapter(generator);
+		adapter.generateFile("temp_diploma.pdf");
+	}
+
+}
