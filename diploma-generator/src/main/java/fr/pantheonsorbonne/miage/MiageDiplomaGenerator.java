060eeb15d43566f20b60f172d7b3d2c7732b8e01
Kenz Touari <kenztouari@icloud.com>
2022-10-23 19:24:22 +0200

    releaseV1

--- diploma-generator/src/main/java/fr/pantheonsorbonne/miage/MiageDiplomaGenerator.java
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/MiageDiplomaGenerator.java
@@ -1,26 +1,10 @@
 package fr.pantheonsorbonne.miage;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
-import java.util.Locale;
-
-import com.google.common.collect.Collections2;
-import com.itextpdf.text.BaseColor;
-import com.itextpdf.text.Chunk;
-import com.itextpdf.text.Font;
-import com.itextpdf.text.FontFactory;
-import com.itextpdf.text.pdf.ColumnText;
-
 import fr.pantheonsorbonne.miage.diploma.NameSnippet;
 import fr.pantheonsorbonne.miage.diploma.DateSnippet;
 import fr.pantheonsorbonne.miage.diploma.DiplomaSnippet;
-
 public class MiageDiplomaGenerator extends AbstractDiplomaGenerator {
 
 	private Student student;
@@ -42,7 +26,7 @@ public class MiageDiplomaGenerator extends AbstractDiplomaGenerator {
 
 	@Override
 	protected Collection<DiplomaSnippet> getDiplomaSnippets() {
-		String studentName = this.student.getName() + " " + this.student.getTitle();
+		String studentName = this.student.getTitle()+ " "+this.student.getName();
 		return Arrays.asList(new DateSnippet(this.date), new NameSnippet(studentName));
 	}
 
